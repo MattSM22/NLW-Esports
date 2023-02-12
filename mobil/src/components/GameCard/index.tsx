5d4c64f6dbf23224ff9f24759c76805add235cfe
MattSM22 <matheusmagdevelop@gmail.com>
2023-02-12 18:37:14 -0300

    updating name of folder mobile to mobil for upload files from mobile

---
+++ mobil/src/components/GameCard/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { TouchableOpacity,
+         TouchableOpacityProps, 
+         ImageBackground, 
+         Text } 
+        from 'react-native';
+
+import { LinearGradient } from 'expo-linear-gradient';
+
+import { styles } from './styles';
+import { THEME } from '../../theme';
+
+export interface GameCardProps {
+  id: string;
+  title: string;
+  _count: {
+    ads: number;
+  };
+  bannerUrl: string;
+}
+
+interface Props extends TouchableOpacityProps {
+  data: GameCardProps;
+}
+
+export function GameCard({ data }: Props) {
+  return (
+    <TouchableOpacity style = {styles.container}>
+      <ImageBackground 
+        style = {styles.cover}
+        source = {{uri: data.bannerUrl}}
+      >
+        
+        <LinearGradient 
+          colors = {THEME.COLORS.FOOTER}
+          style = {styles.footer}
+        >
+          <Text style={styles.name}>
+            {data.title}
+          </Text>
+          <Text style={styles.ads}>
+            {data._count.ads} anúncios
+          </Text>
+
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+}
